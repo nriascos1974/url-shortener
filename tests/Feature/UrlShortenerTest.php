f51fe70ed91c032de0ede0e0054f3ef6f2e8cbda
nriascos1974 <nestorriascos74@gmail.com>
2024-09-16 10:54:26 -0500

    Test  probado

--- tests/Feature/UrlShortenerTest.php
+++ tests/Feature/UrlShortenerTest.php
@@ -13,27 +13,33 @@ class UrlShortenerTest extends TestCase
     // Prueba para verificar que un URL se acorta correctamente
     public function test_store_url()
     {
+        // Se envia una solicitud POST a la ruta para crear un URL
         $response = $this->postJson('/url', [
             'original_url' => 'https://example.com'
         ]);
 
-        
-        $response->assertStatus(201)
-                 ->assertJsonStructure(['short_url']);
+        // Se verifica que el estado de respuesta sea 201 (creado)
+        $response->assertStatus(201);
+
+
+       // Se verifica que la respuesta JSON tenga la estructura correcta
+       $response->assertJsonStructure(['short_url']);
+
     }
 
     // Prueba para verificar la redirección del URL acortado
     public function test_redirect_to_original_url()
     {
-        // Crear un URL acortado en la base de datos
+        // Se crea un URL acortado en la base de datos
         $url = Url::create([
             'original_url' => 'https://example.com',
             'short_url' => 'abc123'
         ]);
 
-        // Verificar que redirige al URL original
+        /// Se envia una solicitud GET para redirigir al URL acortado
         $response = $this->get('/abc123');
 
+        // Se verifica que la respuesta redirige al URL original
         $response->assertRedirect('https://example.com');
     }
 }
